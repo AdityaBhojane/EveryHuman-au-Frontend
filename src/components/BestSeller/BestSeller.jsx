e5dc133e332636358307db79a0bc97fc394e93be
aditya bhojane <bhojaneaditya7@gmail.com>
2024-10-15 19:29:57 +0530

    Fix: add fix Mobile Responsive on home page

--- src/components/BestSeller/BestSeller.jsx
+++ src/components/BestSeller/BestSeller.jsx
@@ -9,20 +9,10 @@ function BestSeller() {
 
   return (
     <div className="hero bg-base-200 min-h-screen select-none">
-      <div className="flex w-[90%] mx-auto max-xl:block lg:flex-row-reverse">
-        <div className="flex gap-2 p-5 ">
-          <img
-            src={data && data[0].images[2].src}
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <img
-            src={data && data[0].images[0].src}
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-        </div>
-        <div className="self-start p-5">
+      <div className="flex w-[90%] mx-auto max-xl:flex-col-reverse">
+        <div className=" p-5 w-[50%] max-xl:w-full ">
           <h1 className="text-6xl font-bold my-5">Best Seller !</h1>
-          <p className="py-6 w-[60%] text-xl">
+          <p className="py-6 w-full text-xl">
             Experience the perfect blend of quality, functionality, and style
             with our. Designed to meet your everyday needs, this versatile
             product is a must-have for anyone looking to enhance their
@@ -31,6 +21,16 @@ function BestSeller() {
           </p>
           <br />
           <button className="btn btn-primary font-bold">Get it Now !</button>
+        </div>
+        <div className="flex gap-2 p-5 w-[50%] max-xl:mx-auto max-lg:w-[70%]">
+          <img
+            src={data && data[0].images[2].src}
+            className="w-1/2 rounded-lg shadow-2xl"
+          />
+          <img
+            src={data && data[0].images[0].src}
+            className="w-1/2 rounded-lg shadow-2xl"
+          />
         </div>
       </div>
     </div>
